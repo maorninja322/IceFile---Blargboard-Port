05f775b90a15f2a88afe77e784ef682a716ee4d3
AlphaTech <alphat3ch@icloud.com>
2015-04-10 20:24:39 -0400

    Update index.php

--- installer/index.php
+++ installer/index.php
@@ -1,7 +1,10 @@
+<!DOCTYPE html>
+<html>
 <html lang="en">
 <head>
 <title>Uploader Installer</title>
-<style>
+<!--Fork me on GitHub banner--><style>#forkongithub a{background:#a;color:#fff;text-decoration:none;font-family:arial,sans-serif;text-align:center;font-weight:bold;padding:5px 40px;font-size:1rem;line-height:2rem;position:relative;transition:0.5s;}#forkongithub a:hover{background:#c11;color:#fff;}#forkongithub a::before,#forkongithub a::after{content:"";width:100%;display:block;position:absolute;top:1px;left:0;height:1px;background:#fff;}#forkongithub a::after{bottom:1px;top:auto;}@media screen and (min-width:800px){#forkongithub{position:fixed;display:block;top:0;right:0;width:200px;overflow:hidden;height:200px;z-index:9999;}#forkongithub a{width:200px;position:absolute;top:60px;right:-60px;transform:rotate(45deg);-webkit-transform:rotate(45deg);-ms-transform:rotate(45deg);-moz-transform:rotate(45deg);-o-transform:rotate(45deg);box-shadow:4px 4px 10px rgba(0,0,0,0.8);}}</style><span id="forkongithub"><a href="https://github.com/StarLand64/IceFile---Blargboard-Port">Fork me on GitHub</a></span>
+<style type="text/css">
 html, body { background: black; color: white; font-family: calibri; }
 div.main {
 	background: grey;
@@ -86,7 +89,8 @@ div#animation a {
 <input type="submit" name="submit" value="Install"></input><hr>
 <b>Important:</b> Only useable for blargboard Plus & XD
 <hr>
-Copyright &copy; StarLand, 2015
+Copyright &copy;2014-<?php echo date("Y"); ?> StarLand<br />
+<a href="https://github.com/StarLand64/IceFile---Blargboard-Port">View This Project on GitHub</a>
 </div>
 </form>
 </body>
