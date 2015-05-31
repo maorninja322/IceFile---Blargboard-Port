a095293210cdf7b86ae767a1d4c36fbd763dc1a9
maor322 <maorneimblarg@gmail.com>
2015-05-31 11:15:56 -0400

    Create uploader.php
    For the plugin

---
+++ Plugin/IceFile/pages/uploader.php
@@ -0,0 +1,30 @@
+<?php
+if (!defined('BLARG')) die();
+MakeCrumbs(array(actionLink("uploader") => __("Uploader")), $links);
+$title = __("Uploader");
+$links = array();
+?>
+	<table class="outline margin">
+		<tbody><tr class="header1">
+			<th>
+				Uploader
+			</th>
+		</tr>
+		<tr class="cell0">
+		<td>
+			<div align="center"><a href="?page=uploaderpublic">Upload Files (Public)</a></div>
+		</td>
+		</tr>
+</tbody></table>
+	<table class="outline margin">
+		<tbody><tr class="header1">
+			<th>
+				Uploaded
+			</th>
+		</tr>
+		<tr class="cell0">
+		<td>
+			<div align="center"><a href="?page=uploadedpublic">Show Uploaded Files</a></div>
+		</td>
+		</tr>
+</tbody></table>
